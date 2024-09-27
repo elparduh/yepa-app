cc03a27a8e3e24c4051587cdf7095c530178c900
elparduh <ticantevicentejuan@outlook.com>
2024-09-26 21:39:51 -0600

    add improvs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:yepa/app/app.dart';
 
-import 'package:yepa/main.dart';
+import 'package:yepa/main_production.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

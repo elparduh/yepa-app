84873d16ff8e710f413e223023077b783f3b3259
elparduh <ticantevicentejuan@outlook.com>
2024-10-31 01:45:49 -0600

    add firestore

--- lib/storage/firestore_dao.dart
+++ lib/storage/firestore_dao.dart
@@ -3,12 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreDao {
   final firestore = FirebaseFirestore.instance;
 
-  Future<void> setDocumentInSubCollection(
-      {required String parentName,
-        required String childName,
-        String? parentUniqueKey,
-        String? childUniqueKey,
-        required Map<String, dynamic> data}) async =>
+  Future<void> setDocumentInSubCollection({
+    required String parentName,
+    required String childName,
+    String? parentUniqueKey,
+    String? childUniqueKey,
+    required Map<String, dynamic> data,
+  }) async =>
       firestore
           .collection(parentName)
           .doc(parentUniqueKey)
@@ -16,33 +17,44 @@ class FirestoreDao {
           .doc(childUniqueKey)
           .set(data);
 
-  Future<QuerySnapshot<Map<String, dynamic>>> getCollection(
-      {required String path, String? uniqueKey}) =>
+  Future<QuerySnapshot<Map<String, dynamic>>> getCollection({
+    required String path,
+    String? uniqueKey,
+  }) =>
       firestore.collection(path).get();
 
-  Future<void> setDocument(
-      {required String name,
-        String? uniqueKey,
-        required Map<String, dynamic> data}) async =>
+  Future<void> setDocument({
+    required String name,
+    String? uniqueKey,
+    required Map<String, dynamic> data,
+  }) async =>
       firestore.collection(name).doc(uniqueKey).set(data);
 
-  Future<Map<String, dynamic>> getDocument(
-      {required String name, required String uniqueKey}) async {
+  Future<Map<String, dynamic>> getDocument({
+    required String name,
+    required String uniqueKey,
+  }) async {
     final docRef = firestore.collection(name).doc(uniqueKey);
     final document = await docRef.get();
     return document.data() as Map<String, dynamic>;
   }
 
-  Future<DocumentSnapshot<Map<String, dynamic>>> getDocumentSnapshot(
-      {required String name, required String uniqueKey}) =>
+  Future<DocumentSnapshot<Map<String, dynamic>>> getDocumentSnapshot({
+    required String name,
+    required String uniqueKey,
+  }) =>
       firestore.collection(name).doc(uniqueKey).get();
 
-  updateDocument(
-      {required String name,
-        String? uniqueKey,
-        required Map<String, dynamic> data}) =>
+  updateDocument({
+    required String name,
+    String? uniqueKey,
+    required Map<String, dynamic> data,
+  }) =>
       firestore.collection(name).doc(uniqueKey).update(data);
 
-  deleteDocument({required String collectionName, String? uniqueKey}) =>
+  deleteDocument({
+    required String collectionName,
+    String? uniqueKey,
+  }) =>
       firestore.collection(collectionName).doc(uniqueKey).delete();
 }

b22b21b9d128b52fc42b7af993466cbbd0caba7b
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-10-30 23:15:45 -0600

    Setup-Firebase-Android - Core (#5)

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:yepa/app/app.dart';
+import 'package:yepa/providers/firebase_provider.dart';
 
 void main() {
+  FirebaseProvider.initialize();
   runApp(const App());
 }
---
+++ lib/providers/firebase_provider.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseProvider {
+
+  static initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Firebase.initializeApp();
+
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
+}
